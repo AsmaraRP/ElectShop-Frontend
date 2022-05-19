9ac773eb66cb1eab81c72950ea5ebd2ee6317815
irvanmnwr <irvanmnwr@gmail.com>
2022-05-19 14:04:52 +0700

    add and slicing responsive admin dashboard page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Manage from "./pages/Manage";
+import Checking from "./pages/Cheking";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="home" element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="manage" element={<Manage />} />
+        <Route path="cheking" element={<Checking />} />
       </Routes>
     </BrowserRouter>
   );
