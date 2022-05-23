5b1904e42958ae5820fda44c53d5f6f6d347e276
irvanmnwr <irvanmnwr@gmail.com>
2022-05-23 12:01:47 +0700

    handle checking page

--- src/stores/reducer/index.js
+++ src/stores/reducer/index.js
@@ -3,10 +3,12 @@ import product from "./product";
 import register from "./register";
 import user from "./user";
 import checkout from "./checkout";
+import transaction from "./transaction";
 
 export default combineReducers({
   product,
   register,
   user,
   checkout,
+  transaction,
 });
